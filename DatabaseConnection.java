ae16de2e10bfb58d637ae7d1011ca4f59124e8f0
thanhdo1043 <150434363+thanhdo1043@users.noreply.github.com>
2025-11-17 09:36:46 +0700

    Add files via upload

---
+++ DatabaseConnection.java
@@ -0,0 +1,23 @@
+package com.departmentstore.util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DatabaseConnection {
+    private static final String URL = "jdbc:mysql://localhost:3306/department_store";
+    private static final String USERNAME = "root";
+    private static final String PASSWORD = "password";
+    
+    static {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException("MySQL JDBC Driver not found", e);
+        }
+    }
+    
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(URL, USERNAME, PASSWORD);
+    }
+}
